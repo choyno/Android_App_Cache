8401f04bf8839cd6f3e0dbc10796346a8df0d6d9
longluo <calmbody@163.com>
2013-08-06 11:05:03 +0800

    Adding Two Projects, LightRead and PhotoCropper, for MemoryCache, increasing the effectcy.

---
+++ LightRead/src/com/longluo/softread/SoftReadApplication.java
@@ -0,0 +1,12 @@
+package com.longluo.softread;
+
+import android.app.Application;
+
+public class SoftReadApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+	}
+}
